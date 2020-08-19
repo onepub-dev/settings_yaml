2433bb522ddbf8946845a18109c72a30bd10286b
Brett Sutton <bsutton@noojee.com.au>
2020-08-19 11:23:32 +1000

    Improved the api doco with examples. Further simplification of the api.

--- lib/src/settings_yaml.dart
+++ lib/src/settings_yaml.dart
@@ -9,14 +9,23 @@ class SettingsYaml {
   String filePath;
   var valueMap = <String, dynamic>{};
 
-  SettingsYaml({@required this.filePath});
-
-  SettingsYaml.fromString(
-      {@required String contents, @required this.filePath}) {
+  /// Loads settings from a string.
+  /// The [content] must be formatted like a standard yaml file would be:
+  ///
+  /// ```
+  /// var settings = SettingsYaml(content: '''
+  /// password: xxxx
+  /// user: xxxx
+  /// ''', filePath: 'mysettings.yaml');
+  /// ```
+  ///
+  /// The [filePath] is the path/file name that will be used when
+  /// save is called.
+  SettingsYaml.fromString({@required String content, @required this.filePath}) {
     /// don't try to load an empty settings file. It will end in tears.
-    if (contents == null || contents.trim().isEmpty) return;
+    if (content == null || content.trim().isEmpty) return;
 
-    _document = loadYamlDocument(contents);
+    _document = loadYamlDocument(content);
 
     var topMap = _document.contents as YamlMap;
 
@@ -25,28 +34,52 @@ class SettingsYaml {
     }
   }
 
-  static SettingsYaml load({String filePath, bool create = false}) {
+  /// Loads a settings file from the give [filePath].
+  /// The [filePath] must point to a file (not a directory).
+  /// The directory component of [filePath] maybe absolute or relative but
+  /// the entire directory path must exist.
+  ///
+  /// If the directory tree of [filePath] doesn't exist then a SettingsYamlException will be thrown.
+  ///
+  /// If you pass [create] = true then if the settings file doesn't already
+  /// exist it will be created.
+  /// [create] defaults to false.
+  ///
+  static SettingsYaml load({@required String filePath}) {
     if (!exists(filePath)) {
-      if (create) {
-        touch(filePath, create: true);
-      } else {
-        throw SettingsYamlException('The yaml file $filePath does not exist');
+      if (!exists(dirname(filePath))) {
+        throw SettingsYamlException(
+            'The directory tree above ${truepath(filePath)} does not exist. Create the directory tree and try again.');
       }
+      touch(filePath, create: true);
     }
 
     var contents = File(filePath).readAsStringSync();
 
-    return SettingsYaml.fromString(contents: contents, filePath: filePath);
+    return SettingsYaml.fromString(content: contents, filePath: filePath);
   }
 
-  void put(String key, String value) {
-    valueMap[key] = value;
-  }
+  // void put(String key, String value) {
+  //   valueMap[key] = value;
+  // }
 
   /// Returns the value for the given key
+  /// ```
+  /// var settings = SettingsYaml.load('mysettings.yaml');
+  /// var password = settings['password'];
+  /// ```
   dynamic operator [](String path) => valueMap[path];
 
-  /// adds a key value piar
+  /// Adds or Updates the given key/value pair.
+  ///
+  /// The value may be a String or a number (int, double);
+  ///
+  /// ```
+  /// var settings = SettingsYaml.load('mysettings.yaml');
+  ///  settings['password'] = 'a new password';
+  /// settings.save();
+  /// ```
+  ///
   void operator []=(String path, dynamic value) => valueMap[path] = value;
 
   // String operator [](String path) {
@@ -63,6 +96,10 @@ class SettingsYaml {
   //     }
   //   }
 
+  /// Saves the settings back to the settings file.
+  /// To avoid a corrupted file we first copy the existing
+  /// settings file to a .bak file.
+  /// If the save fails you may need to manually rename the .bak file.
   void save() {
     var tmp = FileSync.tempFile();
     tmp.write('# SettingsYaml settings file');
