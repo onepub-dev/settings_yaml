51a7b739ab05e614f4f9b983ca69332ea04234ed
Brett Sutton <bsutton@noojee.com.au>
2022-06-09 12:29:02 +1000

    added copyright notices.

--- lib/settings_yaml.dart
+++ lib/settings_yaml.dart
@@ -1,1 +1,7 @@
+/* Copyright (C) S. Brett Sutton - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Brett Sutton <bsutton@onepub.dev>, Jan 2022
+ */
+
 export 'src/settings_yaml.dart';
